b17f4f4c7ac04f3a08df77a9371082304cb2d964
Tp-arvind <arvind.pandey@tyreplex.com>
2025-03-22 18:06:09 +0530

    commit

--- src/warranty/warranty.controller.ts
+++ src/warranty/warranty.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, HttpException, HttpStatus, Query } from '@nestjs/common';
+import { Body, Controller, HttpException, HttpStatus, Query, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { WarrantyService } from './warranty.service';
 import { CreateWarrantyDto } from './dto/create-warranty.dto';
 import { UpdateWarrantyDto } from './dto/update-warranty.dto';
 import { writeFile } from 'fs/promises';
-import { join } from 'path';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname,join} from 'path';
+import { FastifyRequest } from 'fastify';
+import { createWriteStream, mkdirSync } from 'fs';
+
 
 @Controller()
 export class WarrantyController {
@@ -65,69 +70,36 @@ export class WarrantyController {
     }
   }
     
-  @MessagePattern('saveWarranty')
+  /*@MessagePattern('saveWarranty')
   async saveWarranty(@Body() request: any) {
+    console.log(request);
+    try {
+      const files = [];
 
-    try {      
-      let requestType = 'add';
-      
-      if(request.warranty_id) {
-        requestType = 'edit';
-      };
-      console.log(request);
-      console.log(request.file);
-
-      if (request.file) {
-        //const fileBuffer = Buffer.from(request.file.buffer, 'base64');
-        //const filePath = join(__dirname, 'uploads', request.file.originalname);
-        //console.log(fileBuffer);
-        //console.log(filePath);
-
-        // Save the file locally
-        //fs.writeFileSync(filePath, fileBuffer);
-        //console.log(`File saved: ${filePath}`);
+      let part;
+      while ((part = await req.file()) !== undefined) {
+        if (part.file) {
+          // Read file as Buffer
+          const fileBuffer = await part.toBuffer();
+          
+          files.push({
+            originalname: part.filename,
+            mimetype: part.mimetype,
+            size: fileBuffer.length,
+            buffer: fileBuffer.toString('base64'), // Convert to Base64
+          });
+        } 
       }
 
-      //return request;
+      const payload = { ...bodyParams, files };
+      console.log('make Body:', payload);
 
-      // const data = await this.warrantyService.dbUpdater(request, requestType, true);
-      // console.log(data);
-
-      // if (!data) {
-      //   throw new HttpException(
-      //     { data: { status: false, message: 'No Warranty found.' } },
-      //     HttpStatus.NOT_FOUND
-      //   );
-      // }
-
-      // const response = {
-      //   data: {
-      //     status: true,
-      //     message: 'Warranty retrieved successfully',
-      //     warranty: Array.isArray(data) ? data : data.result,
-      //     ...(data.totalRecords && {
-      //       total_records: data.totalRecords,
-      //       pageno: data.currentPage,
-      //       total_pages: data.totalPages,
-      //       pageSize: data.pageSize
-      //     })
-      //   }
-      // };
-
-      // return response;
-
+      // Send to Misc Service via Kafka
+      return this.miscService.saveWarranty(payload);
     } catch (error) {
-      console.error("Controller Error:", error);
-
-      if (error instanceof HttpException) {
-        throw error;
-      }
-
-      throw new HttpException(
-        { data: { status: false, message: 'Something went wrong' } },
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
+      console.error('Error while processing files:', error);
+      throw error;
     }
-  }
+  }*/
 
 }
